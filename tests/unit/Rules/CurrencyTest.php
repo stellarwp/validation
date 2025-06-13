b2c0877992ddb1424bb04afd1ff4a9dc070150b4
Jon Waldstein <jonwaldstein@mac.mynetworksettings.com>
2025-06-13 18:00:51 -0400

    tests: update since tag

--- tests/unit/Rules/CurrencyTest.php
+++ tests/unit/Rules/CurrencyTest.php
@@ -10,7 +10,7 @@ use StellarWP\Validation\Tests\TestCase;
 class CurrencyTest extends TestCase
 {
     /**
-     * @unreleased
+     * @since 1.1.0
      * @dataProvider currencyProvider
      */
     public function testCurrencyValidations($currency, $shouldPass)
